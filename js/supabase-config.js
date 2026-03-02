c9b882640de89ce0b01b28b4ddfc9458cfd68105
Vitor Paes <paes.vitor@yagoo.com.br>
2026-03-02 19:24:00 -0300

    Update supabase-config.js

--- js/supabase-config.js
+++ js/supabase-config.js
@@ -6,7 +6,7 @@
 // ============================================================
 
 const SUPABASE_URL = 'https://uxvtqnlkjkprxjclvbrm.supabase.co';
-const SUPABASE_ANON_KEY = 'sb_publishable_NIke-WDjOGifO2BBGdujsg_WI7rk_ts';
+const SUPABASE_ANON_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6InV4dnRxbmxramtwcnhqY2x2YnJtIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NzI0ODQ2MjQsImV4cCI6MjA4ODA2MDYyNH0.QHUV_F48KCpgGv_Fvc-Lfqkt4kz9gzefMS0Vyh0nSyM';
 
 // Inicializar cliente Supabase
 const supabase = window.supabase.createClient(SUPABASE_URL, SUPABASE_ANON_KEY);
